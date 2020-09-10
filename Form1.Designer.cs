e6f28e3ea854bb0bc84c9b2389bcf33490453a5f
aksmaxn4 <70766698+aksmaxn4@users.noreply.github.com>
2020-09-10 23:34:19 +0500

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,349 @@
+﻿namespace HAM
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 40);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(363, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextBox1_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(331, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Введите слово, состоящее из a и b c четным количеством букв";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(9, 115);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(363, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 96);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Текущее слово:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(407, 38);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "ОК";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(6, 19);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Выполнить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 72);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "label3";
+            this.label3.Visible = false;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Enabled = false;
+            this.radioButton1.Location = new System.Drawing.Point(125, 19);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(103, 17);
+            this.radioButton1.TabIndex = 7;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Автоматически";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Enabled = false;
+            this.radioButton2.Location = new System.Drawing.Point(125, 43);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(75, 17);
+            this.radioButton2.TabIndex = 8;
+            this.radioButton2.Text = "По шагам";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Location = new System.Drawing.Point(12, 139);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(235, 70);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Enabled = false;
+            this.richTextBox1.Location = new System.Drawing.Point(272, 143);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(158, 80);
+            this.richTextBox1.TabIndex = 10;
+            this.richTextBox1.Text = "Задание:\nA={a,b}. Пусть слово P имеет чётную длину (0, 2, 4, …). Удалить правую п" +
+    "оловину этого слова.";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(452, 80);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(117, 26);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Описание алгоритма:\r\n\r\n";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.White;
+            this.label5.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label5.Location = new System.Drawing.Point(452, 100);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(49, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "1) *A->a*";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(452, 115);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(49, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "2) *B->b*";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.White;
+            this.label7.Location = new System.Drawing.Point(452, 128);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(34, 13);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "3) *|->";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.White;
+            this.label8.Location = new System.Drawing.Point(452, 142);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(38, 13);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "4)A2->";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.BackColor = System.Drawing.Color.White;
+            this.label9.Location = new System.Drawing.Point(452, 155);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(41, 13);
+            this.label9.TabIndex = 16;
+            this.label9.Text = "5) B2->";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.BackColor = System.Drawing.Color.White;
+            this.label10.Location = new System.Drawing.Point(452, 168);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(47, 13);
+            this.label10.TabIndex = 17;
+            this.label10.Text = "6) a->A1";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.BackColor = System.Drawing.Color.White;
+            this.label11.Location = new System.Drawing.Point(452, 182);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(47, 13);
+            this.label11.TabIndex = 18;
+            this.label11.Text = "7) b->B1";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.BackColor = System.Drawing.Color.White;
+            this.label12.Location = new System.Drawing.Point(452, 197);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(54, 13);
+            this.label12.TabIndex = 19;
+            this.label12.Text = "8) 1A->A1";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.BackColor = System.Drawing.Color.White;
+            this.label13.Location = new System.Drawing.Point(452, 210);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(54, 13);
+            this.label13.TabIndex = 20;
+            this.label13.Text = "9) 1B->B1";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.BackColor = System.Drawing.Color.White;
+            this.label14.Location = new System.Drawing.Point(447, 225);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(52, 13);
+            this.label14.TabIndex = 21;
+            this.label14.Text = "10) 11->2";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.BackColor = System.Drawing.Color.White;
+            this.label15.Location = new System.Drawing.Point(447, 238);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(38, 13);
+            this.label15.TabIndex = 22;
+            this.label15.Text = "11) ->*";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(739, 297);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Нормальные алгоритмы Маркова";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label15;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HAM
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox1_KeyPress(object sender, KeyPressEventArgs e)//запрет ввода символов кроме a,b и BackSpace
+        {
+            if (e.KeyChar.ToString() == "a" || e.KeyChar.ToString() == "b" || e.KeyChar == (char)Keys.Back) e.Handled = false;
+            else e.Handled = true;
+        }
+
+        public void NAM()//запуск НАМ
+        {
+            FirstRule("*A", "a*");//запуск первого правила со строками, которую хотим заменить(первый аргумент) и которой хотим заменить(второй аргумент)
+           
+        }
+
+        public void FirstRule(string substring,string rep)//первое правило
+        {
+            if (textBox2.Text.Contains(substring))//проверка на содержание заменяемой строки
+            {
+                if (radioButton1.Checked == false)//проверка, как выполняется алгоритм: по шагам или автоматически
+                    label5.BackColor = Color.Red;//подсвечивание соответствующего правила
+                ChangeAndToStart(substring, rep);//замена заменяемой строки на строку-заменитель
+            }
+            else SecondRule("*B", "b*");//переход в следующее правило
+            //справедливо для всех остальных правил
+           
+        }
+        public void SecondRule(string substring, string rep)//второе правило
+        {
+            if (textBox2.Text.Contains(substring)) { if (radioButton1.Checked == false) label6.BackColor = Color.Red; ChangeAndToStart(substring, rep); }
+            else ThirdRule("*", "");
+        }
+        public void ThirdRule(string substring, string rep)//терминальное правило
+        {
+            if (textBox2.Text.Contains(substring))
+            {
+                string original = textBox2.Text;
+                int ind = original.IndexOf(substring);
+                textBox2.Text = original.Remove(ind, substring.Length).Insert(ind, rep);
+                button2.Enabled = false;
+                button1.Enabled = true;
+                radioButton1.Enabled = false;
+                radioButton2.Enabled = false;
+                if (radioButton1.Checked == false)
+                    label7.BackColor = Color.Red;
+
+                Finish();//функция, отвечающая за конец выполнения алгоритма
+                return;
+            }
+            else FourthRule("A2", "");
+        }
+        public void FourthRule(string substring, string rep)//четвертое правило
+        {
+            if (textBox2.Text.Contains(substring)) { if (radioButton1.Checked == false) label8.BackColor = Color.Red; ChangeAndToStart(substring, rep); }
+            else FifthRule("B2", "");
+        }
+        public void FifthRule(string substring, string rep)//пятое правило
+        {
+            if (textBox2.Text.Contains(substring)) { if (radioButton1.Checked == false) label9.BackColor = Color.Red; ChangeAndToStart(substring, rep); }
+            else SixthRule("a", "A1");
+        }
+        public void SixthRule(string substring, string rep)//шестое правило
+        {
+            if (textBox2.Text.Contains(substring)) { if (radioButton1.Checked == false) label10.BackColor = Color.Red; ChangeAndToStart(substring, rep); }
+            else SeventhRule("b", "B1");
+        }
+        public void SeventhRule(string substring, string rep)//седьмое правило
+        {
+            if (textBox2.Text.Contains(substring)) { if (radioButton1.Checked == false) label11.BackColor = Color.Red; ChangeAndToStart(substring, rep); }
+            else EigthRule("1A", "A1");
+        }
+        public void EigthRule(string substring, string rep)//восьмое правило
+        {
+            if (textBox2.Text.Contains(substring)) { if (radioButton1.Checked == false) label12.BackColor = Color.Red; ChangeAndToStart(substring, rep); }
+            else NinethRule("1B", "B1");
+        }
+        public void NinethRule(string substring, string rep)//девятое правило
+        {
+            if (textBox2.Text.Contains(substring)) { if(radioButton1.Checked==false) label13.BackColor = Color.Red; ChangeAndToStart(substring, rep); }
+            else TenthRule("11", "2");
+        }
+        public void TenthRule(string substring, string rep)//десятое правило
+        {
+            if (textBox2.Text.Contains(substring)) { if (radioButton1.Checked == false) label14.BackColor = Color.Red; ChangeAndToStart(substring, rep); }
+            else EleventhRule();
+        }
+        public void EleventhRule()//11 правило
+        {
+            textBox2.Text = textBox2.Text.Insert(0, "*");//вставка в начало строки *
+            if (radioButton1.Checked == false)
+                label15.BackColor = Color.Red;
+            if(radioButton1.Checked==true) NAM();//проверка если поставлен автоматический режим
+        }
+
+        public void ChangeAndToStart(string substring,string rep)//функция замены одной строки на другую
+        {
+            string original = textBox2.Text;
+            int ind = original.IndexOf(substring);
+            textBox2.Text = original.Remove(ind, substring.Length).Insert(ind, rep);//удление по индексу заменяемой строки и вставка строки-заменителя
+            if (radioButton1.Checked == true) NAM();
+            
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Length % 2 == 0)//проверка на четное кол-во букв
+            {
+                textBox2.Text = textBox1.Text;
+                label3.Text = "Исходное слово: " + textBox1.Text;//ввод исходного слова
+                textBox1.Clear();
+                button2.Enabled = true;
+                
+                radioButton1.Enabled = true;
+                radioButton2.Enabled = true;
+                
+                label3.Visible = true;
+            }
+            else MessageBox.Show("Введите четное количество букв");
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            label5.BackColor = Color.White;//окрашивание всех правил в белый цвет
+            label6.BackColor = Color.White;
+            label7.BackColor = Color.White;
+            label8.BackColor = Color.White;
+            label9.BackColor = Color.White;
+            label10.BackColor = Color.White;
+            label11.BackColor = Color.White;
+            label12.BackColor = Color.White;
+            label13.BackColor = Color.White;
+            label14.BackColor = Color.White;
+            label15.BackColor = Color.White;
+            NAM();//запуск алгоритма
+        }
+        private void Finish()
+        {
+            if (textBox2.Text.Length % 2 == 0)
+            {
+                if (MessageBox.Show("Алгоритм завершен. Использовать алгоритм для полученного слова?", "Успех", MessageBoxButtons.OKCancel) == DialogResult.OK)//предложение использовать полученное слово в качестве исходного
+                {
+                    label3.Text = "Исходное слово: " + textBox2.Text;
+                    button2.Enabled = true;
+                    radioButton1.Enabled = true;
+                    radioButton2.Enabled = true;
+                }
+
+            }
+            else MessageBox.Show("Алгоритм завершен!");
+        }
+    }
+}
